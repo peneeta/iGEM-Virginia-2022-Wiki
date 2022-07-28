fc50dca9c021ca0d6e5d4031276d5b066e8cc46c
peneetawojcik <peneetaw@gmail.com>
2022-07-27 22:24:33 -0400

    fix jquery

--- static/components/counter-script.js
+++ static/components/counter-script.js
@@ -1,6 +1,30 @@
 import { CountUp } from '../countUp.min.js';
 
-window.onload = function() {
-  var countUp = new CountUp('myTargetElement', 17900000, {useEasing: true}, {enableScrollSpy: true });
-  countUp.start();
+var options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ',',
+    decimal: '.',
+};
+
+var counterParent = 
+document.getElementById('counter-parent');
+var children = counterParent.children;
+
+function countStart(){
+    $('#counter-parent').addClass('active');
+    for (var i = 0; i < children.length; i++){
+        var item = document.getElementById(children[i].id).querySelector('.counter');
+
+        var countValue = item.getAttribute('data-value');
+
+        var countUp = new CountUp(item, 0, countValue, 0, 2, options);
+        if (!countUp.error) {
+            countUp.start();
+        }
+    }
 }
+
+ScrollReveal().reveal('.counter-section', {
+    beforeReveal: countStart
+})
