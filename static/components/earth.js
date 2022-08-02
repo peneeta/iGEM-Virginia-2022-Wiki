f16e8370d72c4fe9463f07781d75a22e2551f402
peneetawojcik <peneetaw@gmail.com>
2022-08-02 13:03:37 -0400

    make globe w/h responsive

--- static/components/earth.js
+++ static/components/earth.js
@@ -1,11 +1,3 @@
-    // Gen random data
-    const N = 300;
-    const gData = [...Array(N).keys()].map(() => ({
-      lat: (Math.random() - 0.5) * 180,
-      lng: (Math.random() - 0.5) * 360,
-      size: Math.random() / 3,
-      color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-    }));
 
     const globeDOM = document.querySelector("[data-js-globe]");
 
@@ -15,9 +7,14 @@
     myGlobe.controls().autoRotateSpeed = 1;
     myGlobe.controls().enableZoom = false;
 
+    window.addEventListener('resize', (event) => {
+      myGlobe.width([event.target.innerWidth])
+      myGlobe.height([event.target.innerHeight])
+    });
+
     myGlobe
       .globeImageUrl('https://static.igem.wiki/teams/4477/wiki/images/earth-night.jpg')
-      .pointsData(gData)
+
       .pointAltitude('size')
       .pointColor('color')
       .showAtmosphere(true)
